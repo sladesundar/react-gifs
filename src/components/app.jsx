62e60cf8a9e814ff3c08bf63e3338f3efb461c35
Slade Sundar <slade@savoirapp.com>
2017-12-05 10:38:54 +0000

    Gify done after looking at example by seb

---
+++ src/components/app.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import SearchBar, { DEFAULT_SEARCH } from './search_bar';
+import Gif from './gif.jsx';
+import GifList from './gif_list.jsx';
+import giphy from 'giphy-api';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "xT9IgDEI1iZyb2wqo8"
+    };
+
+    this.search(DEFAULT_SEARCH);
+  }
+
+  search = (query) => {
+    giphy({ apiKey: 'g2bRQw0FpQ1qiXxv7UdwwZS8KeyIwYD1', https: true })
+      .search({
+        q: query,
+        rating: 'g',
+        limit: 10
+      }, (error, result) => {
+        this.setState({
+          gifs: result.data
+        });
+      });
+  }
+
+  selectGif = (id) => {
+    this.setState({
+      selectedGifId: id
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="left-scene">
+          <SearchBar searchFunction={this.search} />
+          <div className="selected-gif">
+            <Gif id={this.state.selectedGifId} />
+          </div>
+        </div>
+        <div className="right-scene">
+          <GifList gifs={this.state.gifs} selectGif={this.selectGif} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+
---
+++ src/components/search_bar.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+
+
+const DEFAULT_SEARCH = "homer";
+
+class SearchBar extends Component {
+  handleUpdate = (event) => {
+    this.props.searchFunction(event.target.value);
+  }
+
+
+
+  render() {
+    return (
+      <input
+        type="text"
+        className="form-control form-search"
+        defaultValue={DEFAULT_SEARCH}
+        onChange={this.handleUpdate}
+      />
+    );
+  }
+}
+
+export default SearchBar;
+export { DEFAULT_SEARCH };
